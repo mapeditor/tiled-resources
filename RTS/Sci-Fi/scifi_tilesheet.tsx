12c14847162dd934b6435e85c7af64f7c55394cf
Andre van Tonder <drevtonder@gmail.com>
2016-12-23 10:44:21 +0800

    Add files via upload

---
+++ RTS/Sci-Fi/scifi_tilesheet.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="scifi_tilesheet" tilewidth="64" tileheight="64" tilecount="126">
+ <image source="scifi_tilesheet.png" width="1152" height="448"/>
+</tileset>
